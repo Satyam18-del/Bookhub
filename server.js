40de346b14d34a56cdeb32c0c26868b982a8e075
Satyam18-del <satyamuchiha18@gmail.com>
2026-07-07 15:20:52 +0530

    Fix Render Port

--- server.js
+++ server.js
@@ -343,4 +343,8 @@ app.delete("/books/:id", (req, res) => {
   );
 });
 
-app.listen(3000, () => console.log("Server Running"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server Running on Port ${PORT}`);
+});
